97788d34be9e1436a589f252a15538e2d23169c4
mosalut <ludaoyuan@mosalut.com>
2021-05-16 14:11:54 +0800

    Deploy upgradeable

--- deploy.js
+++ deploy.js
@@ -11,16 +11,14 @@ main().then(() => process.exit(0)).catch((error) => {
 var aitn;
 async function deployAitn() {
 	const Aitn = await hre.ethers.getContractFactory("Aitn");
-	aitn = await Aitn.deploy("AI crypto machine token", "AITN");
-	await aitn.deployed();
+	aitn = await upgrades.deployProxy(Aitn, ["Artificial Intelligence Technology Network", "AITN"]);
 
 	await console.log("aitn deployed to:", aitn.address);
 }
 
 async function deployCryptoMachine(addrs) {
 	const CryptoMachine = await hre.ethers.getContractFactory("CryptoMachine");
-	const cryptoMachine = await CryptoMachine.deploy("CryptoMachine", "AIMC", aitn.address);
-	await cryptoMachine.deployed();
+	const cryptoMachine = await upgrades.deployProxy(CryptoMachine, ["Crypto Machine", "AIMC", aitn.address]);
 
 	await console.log("cryptoMachine deployed to:", cryptoMachine.address);
 }
